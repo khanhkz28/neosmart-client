a6bf0f657ddb644abf9f06779b840c32b14ca42b
HoaiPhong1411 <96569661+HoaiPhong1411@users.noreply.github.com>
2022-05-19 11:44:36 +0700

    push

--- resources/views/client/contant.blade.php
+++ resources/views/client/contant.blade.php
@@ -109,8 +109,6 @@
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.5.0/socket.io.js"></script>
   <script>
-    var eee = 'chao ban';
-    console.log(eee);
     var socket = io('http://localhost:6001', {
       transports: ["websocket", "polling", "flashsocket"],
     });
